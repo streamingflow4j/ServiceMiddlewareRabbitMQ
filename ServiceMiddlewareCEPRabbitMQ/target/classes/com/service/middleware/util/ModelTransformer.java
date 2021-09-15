6af7c58892e175882ef75f7eba3854e25bf68fd5
herbertt <herberttb@gmail.com>
2021-09-15 12:27:01 -0300

    loggin adjust

--- ServiceMiddlewareCEPRabbitMQ/target/classes/com/service/middleware/util/ModelTransformer.java
+++ ServiceMiddlewareCEPRabbitMQ/target/classes/com/service/middleware/util/ModelTransformer.java
@@ -7,6 +7,8 @@ import java.io.Serializable;
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -19,6 +21,8 @@ public class ModelTransformer  implements Serializable{
 	
 	
 	private static final long serialVersionUID = 1L;
+	
+	private static final Logger log = LoggerFactory.getLogger(ModelTransformer.class);
 	
 	@Autowired
 	MonitorEventHandler monitorEventHandler;
@@ -34,7 +38,8 @@ public class ModelTransformer  implements Serializable{
 			}
 			monitorEventHandler.createRequestMonitorExpression(myEntity);
 		} catch (Exception e) {
-			System.err.println("Não é permitido inserir Entidade! ====>>> "+ e.getMessage());
+			// TODO: handle exception
+			log.error("Não é permitido inserir Entidade! ====>>> "+ e.getMessage());
 		}
 		
 		return myEntity;
