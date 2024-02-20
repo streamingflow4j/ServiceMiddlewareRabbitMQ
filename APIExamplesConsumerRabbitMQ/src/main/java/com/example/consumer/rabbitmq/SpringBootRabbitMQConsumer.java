8a19d4bda50b68978e44537aa134c9ab09f9aa36
DESKTOP-J7F8RBI\Usuario <herbertt.diniz@cnj.jus.br>
2024-02-20 19:05:54 -0300

    ajustment to make user guest remote avaliable with { loopback_users, [] } in rabbitmq.config

--- APIExamplesConsumerRabbitMQ/src/main/java/com/example/consumer/rabbitmq/SpringBootRabbitMQConsumer.java
+++ APIExamplesConsumerRabbitMQ/src/main/java/com/example/consumer/rabbitmq/SpringBootRabbitMQConsumer.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootRabbitMQConsumer {
 
 	public static void main(String[] args) {
-		System.setProperty( "server.port","8081");
 		SpringApplication.run(SpringBootRabbitMQConsumer.class, args);
 	}
 
