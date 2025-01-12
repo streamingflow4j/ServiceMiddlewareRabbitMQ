7e7ab0d38c6ed95cf0aec51e1dc0d036553d4973
DESKTOP-J7F8RBI\Usuario <herberttb@gmail.com>
2025-01-12 17:59:27 -0300

    code refactory test

---
+++ StreamingFlow4JRabbitMQ/src/test/java/com/service/middleware/listener/ConsumerServiceActivatorTest.java
@@ -0,0 +1,48 @@
+package com.service.middleware.listener;
+
+import com.service.middleware.cep.handler.MonitorEventHandler;
+import com.service.middleware.model.Entity;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
+
+import static org.mockito.Mockito.*;
+
+class ConsumerServiceActivatorTest {
+
+    @Mock
+    private MonitorEventHandler monitorEventHandler;
+
+    @InjectMocks
+    private ConsumerServiceActivator consumerServiceActivator;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testListenQueueA() throws Exception {
+        // Arrange
+        String json = "{\"id\":\"1\",\"type\":\"test\",\"attributes\":[{\"name\":\"attr1\",\"value\":\"10\"}]}";
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.setContentType("application/json");
+        Message message = new Message(json.getBytes(), messageProperties);
+        Entity entity = objectMapper.readValue(json, Entity.class);
+
+        // Act
+        consumerServiceActivator.listenQueueA(message);
+
+        // Assert
+        Assert.assertEquals("1", entity.getId());
+        //verify(monitorEventHandler, times(1)).getNumEventsHandled();
+    }
+}
